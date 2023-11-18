94a473e96ee03e3dca0fb61cfafd357a890abf86
nabiSoftware huseynov <huseynovnabi2002+demo@gmail.com>
2023-11-18 14:19:39 +0000

    appJs

--- fourProjects/app.js
+++ fourProjects/app.js
@@ -1,8 +1,30 @@
 "use strict";
 
 $(document).ready(function () {
-    $("#hamburgerMenu").click(function () {
-        $("nav a").slideToggle();
-        $(this).toggleClass("fa-bars fa-times");
-    });
+
+    // ekran yuklenende 992px genislikden kicik ve ya beraber olanda isleyir bu funksiyalar
+    if ($(window).width() <= 992) {
+        $("#hamburgerMenu").click(function () {
+
+            // - linklerin gizledilib ve ya gorsedilmesi
+            $("nav a").slideToggle();
+
+            // - icon change
+            $(this).toggleClass("fa-bars fa-times");
+        });
+
+        $("nav a").click(function () {
+
+            // - linklerin yuxariya dogru gizledilmesi
+            $("nav a").slideUp();
+
+            // - cancel iconun legv edib menu iconun elave edir 
+            $("#hamburgerMenu").removeClass("fa-times").addClass("fa-bars");
+
+        });
+    }
 });
+
+
+
+
